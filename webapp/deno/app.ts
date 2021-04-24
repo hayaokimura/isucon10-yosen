1d253e495a806dd6ed2906b0892ccfa33577877c
MATSUU Takuto <matsuu@gmail.com>
2021-04-25 07:50:42 +0900

    fix organ url
    404 not found github.com/denjucks/organ

--- webapp/deno/app.ts
+++ webapp/deno/app.ts
@@ -1,7 +1,7 @@
 import { Application, Router, helpers } from "https://deno.land/x/oak/mod.ts";
 import { Client } from "https://deno.land/x/mysql/mod.ts";
 import { sprintf } from "https://deno.land/std/fmt/printf.ts";
-import { organ } from "https://raw.githubusercontent.com/denjucks/organ/master/mod.ts";
+import { organ } from "https://deno.land/x/organ/mod.ts";
 import { camelCase } from "https://deno.land/x/case/mod.ts";
 import { parse } from "https://deno.land/std/encoding/csv.ts";
 import { multiParser } from "https://deno.land/x/multiparser/mod.ts";
