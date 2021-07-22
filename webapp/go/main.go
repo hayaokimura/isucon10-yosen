1b6fb590b84455ac185d94983553fe5612d01111
hayaokimura <asahiprop01@gmail.com>
2021-07-22 17:24:56 +0900

    fix

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -701,7 +701,7 @@ func postEstate(c echo.Context) error {
 		})
 	}
 
-	query := `INSERT INTO estate (id, name, description, thumbnail, address, latitude, longitude, rent, door_height, door_width, features, popularity) VALUES (:id, :name, :description, :thumbnail, :address,: latitude, :longitude, :rent, :door_height, :door_width, :features, :popularity)`
+	query := `INSERT INTO estate (id, name, description, thumbnail, address, latitude, longitude, rent, door_height, door_width, features, popularity) VALUES (:id, :name, :description, :thumbnail, :address, :latitude, :longitude, :rent, :door_height, :door_width, :features, :popularity)`
   _, err = db.NamedExec(query, estates)
 
 	if err != nil {
